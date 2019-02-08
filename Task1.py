8970780b5a508b5ae60f8bd1b8e96cca5d5b9241
zorawar123 <zorawar.singh@dal.ca>
2019-02-07 19:04:30 -0800

    Task Updated

--- Task1.py
+++ Task1.py
@@ -4,5 +4,5 @@ def tuple_func():
     for i in fout:
         i = i.split(",")
         string = (i[2],i[4],i[6],i[7]) # A tuple of (STR_NAME,FULL_NAME,FROM_STR,TO_STR)
-        print(string)  
+        print(tuple(string))  
 tuple_func()
--- Task2.py
+++ Task2.py
@@ -1,23 +1,23 @@
 street1=[] # defining 2 list
 Roads=[]
 def func(): # Fuction
-  file1=open('Street_Centrelines.csv',r) # Reading and opening both files
-  file2=open('Bus_Stops.csv',r) 
-    for i in file2:  
-      i=i.strip()
-      word1=sent.split(',') # seprating each word by strip and splitting by using ","
-        if word1[7]=='Accessible': # Comparing the string with string 
-	  if word1[6] not in street1: # again comapring the word 6 with street1 if its not there ,it will append in road
-	    road=word1[6] # as the 6th word is not road, we will add word 6 in variable road
-	      street1.append(road.lower()) # In the list we append the road in lower case letter
-	else:
-	  pass
-	for j in file1:
-	  j =j.strip()
-	  word = j.split(',') # seprating each word by comma
-	  if word2[10]=='ARTERIAL': # comapiring 10th word with given string
-	    street2 =word2[2] # put the word in the 3rd position in street
-	  if street2.lower() in street1: # comapiring the word in 3rd position with street
-	    Roads.append(street)
-	return(Roads)
+  file1=open('Street_Centrelines.csv') # Reading and opening both files
+  file2=open('Bus_Stops.csv') 
+  for i in file2:  
+    i=i.strip()
+    word1=i.split(',') # seprating each word by strip and splitting by using ","
+    if word1[7]=='Accessible': # Comparing the string with string 
+      if word1[6] not in street1: # again comapring the word 6 with street1 if its not there ,it will append in road
+        road=word1[6] # as the 6th word is not road, we will add word 6 in variable road
+        street1.append(road.upper()) # In the list we append the road in upper  case letter
+    else:
+      pass
+    for j in file1:
+      j =j.strip()
+      word2 = j.split(',') # seprating each word by comma
+      if word2[10]=='ARTERIAL': # comapiring 10th word with given string
+        street2 =word2[2] # put the word in the 3rd position in street
+        if street2.upper() in street1: # comapiring the word in 3rd position with street in upper case
+          Roads.append(street2)
+        return(Roads)
 print(func())
