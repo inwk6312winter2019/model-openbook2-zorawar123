44679ea0df6e60b15aedf92a8bdd498349c57e89
zorawar123 <zorawar.singh@dal.ca>
2019-02-07 19:09:41 -0800

    Task Updated

---
+++ Task1_new.py
@@ -0,0 +1,8 @@
+fout = open('Street_Centrelines.csv','r') #It will open and perform read operation in file StreetCenterlines.csv
+
+def tuple_func():
+    for i in fout:
+        i = i.split(",")
+        string = (i[2],i[4],i[6],i[7]) # A tuple of (STR_NAME,FULL_NAME,FROM_STR,TO_STR)
+        print(tuple(string))  
+tuple_func()
